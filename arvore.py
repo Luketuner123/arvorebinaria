6cefd89808763be3d1a2969694112ef6a4e32c10
Luketuner123 <102562382+Luketuner123@users.noreply.github.com>
2023-07-10 03:57:17 -0300

    Update arvore.py
    Corrigi o algoritmo quando inserir um número menor que a raiz

--- arvore.py
+++ arvore.py
@@ -1,3 +1,5 @@
+#Vizualizador em http://btv.melezinek.cz/binary-search-tree.html
+
 class Node():
 
     def __init__(self, dado = None, esquerda = None, direita = None):
@@ -17,33 +19,32 @@ class BST():
             self.raiz.dado = dado
             return True
         
+        #Novo node recebe um node com o dado passado pelo usuário
         novo_node = Node(dado=dado,esquerda=None,direita=None)
         
         #Ir para a ESQUERDA da raiz
-        atual = self.raiz
+        node_atual = self.raiz
         while True:
-            if (dado <= self.raiz.dado):
+            if (dado <= node_atual.dado):
 
-                if (atual.esquerda is None):
-                    atual.esquerda = novo_node
+                if (node_atual.esquerda is None):
+                    node_atual.esquerda = novo_node
                     return False
                 
-                elif (dado <= atual.esquerda.dado):
-                    atual = atual.esquerda
-                
-                # Condição dado > node --> ir para a direita
-                elif (dado > atual.esquerda.dado):
-                    
-                    if atual.esquerda.direita is None:
-                        atual.esquerda.direita = novo_node
-                        return False
-                    else:
-                        atual = atual.esquerda.direita
+                else:
+                    node_atual = node_atual.esquerda
             
-            # Condição para a direita
             else:
-                ...
+                if (node_atual.direita is None):
+                    node_atual.direita = novo_node
+                    return False
+                
+                else: 
+                    node_atual = node_atual.direita
+                
 
+                
+           
 
         
 
@@ -53,12 +54,20 @@ if __name__ == '__main__':
     arvore = BST()
     arvore.inserir(50)
     arvore.inserir(30)
+    arvore.inserir(20)
+    arvore.inserir(10)
+    arvore.inserir(25)
     arvore.inserir(40)
-    arvore.inserir(49)
 
 
     print(arvore.raiz.dado)
     print(arvore.raiz.esquerda.dado)
+    print(arvore.raiz.esquerda.esquerda.dado)
+    print(arvore.raiz.esquerda.esquerda.esquerda.dado)
+    print(arvore.raiz.esquerda.esquerda.direita.dado)
     print(arvore.raiz.esquerda.direita.dado)
-    print(arvore.raiz.esquerda.direita.esquerda)
+
+
+
+    # print(arvore.raiz.esquerda.direita.esquerda)
     # print(arvore.raiz.esquerda.direita.direita.dado)
